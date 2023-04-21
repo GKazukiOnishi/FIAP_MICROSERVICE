f1976b0da38243e1756dece80dc97939fc531f1d
Logon Aluno <logonrmlocal@fiap.com.br>
2023-04-20 22:57:36 -0300

    Aula de microserviços - Projeto produto - View, Handler, Post

--- produto/src/main/java/com/fiap/produto/controllers/ProdutoController.java
+++ produto/src/main/java/com/fiap/produto/controllers/ProdutoController.java
@@ -4,12 +4,19 @@ import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.fiap.produto.modelo.PaginacaoModel;
 import com.fiap.produto.modelo.ProdutoModel;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class ProdutoController {
@@ -25,5 +32,16 @@ public class ProdutoController {
 		
 		return modelView;
 	}
+
+	@PostMapping("/create")
+	public ResponseEntity<ProdutoModel> create(@Valid @RequestBody ProdutoModel model) {
+		return new ResponseEntity(model, HttpStatus.CREATED);
+	}
 	
+	@GetMapping("/produtos/create")
+	public ModelAndView create() {
+		ModelAndView modelView = new ModelAndView("produtos/create");
+		return modelView;
+	}
+
 }
--- produto/src/main/java/com/fiap/produto/modelo/ProdutoModel.java
+++ produto/src/main/java/com/fiap/produto/modelo/ProdutoModel.java
@@ -2,11 +2,30 @@ package com.fiap.produto.modelo;
 
 import java.math.BigDecimal;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 public class ProdutoModel {
 
 	private Long id;
+
+	@NotNull(message = "O campo nome é obrigatório") // Para reclamar quando estiver null, obrigatório
+	@NotBlank(message = "O nome não deve ser vazio")
 	private String nome;
+
+	@NotBlank(message = "É necessário informar a descrição") // Para reclamar quando for null ou uma string com
+																// espaço/vazia
+	private String descricao;
+
 	private BigDecimal valor;
+
+	// AAAA0000A
+	@Pattern(regexp = "[A-Z]{4}[0-9]{4}[A-Z]{1}", message = "Padrão de código inválido")
+	private String codigo;
+
+	@Min(value = 1, message = "É necessário ter ao menos uma unidade")
 	private Integer quantidade;
 	private Integer status;
 
@@ -55,6 +74,22 @@ public class ProdutoModel {
 
 	public void setStatus(Integer status) {
 		this.status = status;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
 	}
 
 }
