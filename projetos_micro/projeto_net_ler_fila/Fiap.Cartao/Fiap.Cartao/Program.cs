a59414d0ff1ac7a777d0221c42697e0b4112610e
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-24 21:55:24 -0300

    Projetos microserviços

---
+++ projetos_micro/projeto_net_ler_fila/Fiap.Cartao/Fiap.Cartao/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Fiap.Cartao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
