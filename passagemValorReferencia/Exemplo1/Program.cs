019d6184b76c833704b4611795dd495b6e81038b
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-14 20:39:41 -0300

    Aula de microserviços - Redis

---
+++ passagemValorReferencia/Exemplo1/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        int idade = 10;
+
+        Incrementar(idade);
+        Console.WriteLine("Após o método " + idade);
+
+        IncrementarReferencia(ref idade);
+        Console.WriteLine("Após o método " + idade);
+
+        void Incrementar(int idade)
+        {
+            idade = idade + 1;
+            Console.WriteLine("Incremento no parâmetro por valor: " + idade);
+        }
+
+        void IncrementarReferencia(ref int idade)
+        {
+            idade = idade + 1;
+            Console.WriteLine("Incremento no parâmetro por referência: " + idade);
+        }
+    }
+}
